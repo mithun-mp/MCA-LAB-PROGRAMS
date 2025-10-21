7df6e46193f2df076556f553c8ee9557af280094
mithun-mp <mithun.m.p.mail@gmail.com>
2025-10-21 13:06:46 +0530

    Create 7-set.c

---
+++ Semester1/DataStructure/7-set.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define MAX 50
+
+int main() {
+    int U[MAX], A[MAX], B[MAX];
+    int bitA[MAX] = {0}, bitB[MAX] = {0};
+    int nU, nA, nB, i, j;
+    printf("Enter number of elements in Universal Set: ");
+    scanf("%d", &nU);
+    printf("Enter elements of Universal Set:\n");
+    for(i = 0; i < nU; i++) scanf("%d", &U[i]);
+    printf("Enter number of elements in Set A: ");
+    scanf("%d", &nA);
+    printf("Enter elements of Set A:\n");
+    for(i = 0; i < nA; i++) scanf("%d", &A[i]);
+    printf("Enter number of elements in Set B: ");
+    scanf("%d", &nB);
+    printf("Enter elements of Set B:\n");
+    for(i = 0; i < nB; i++) scanf("%d", &B[i]);
+    for(i = 0; i < nU; i++) {
+        for(j = 0; j < nA; j++) if(U[i] == A[j]) bitA[i] = 1;
+        for(j = 0; j < nB; j++) if(U[i] == B[j]) bitB[i] = 1;
+    }
+    int unionSet[MAX], intersectionSet[MAX], differenceSet[MAX];
+    for(i = 0; i < nU; i++) unionSet[i] = bitA[i] | bitB[i];
+    for(i = 0; i < nU; i++) intersectionSet[i] = bitA[i] & bitB[i];
+    for(i = 0; i < nU; i++) differenceSet[i] = bitA[i] & (!bitB[i]);
+    printf("\nUnion (A ∪ B) Bit String: ");
+    for(i = 0; i < nU; i++) printf("%d", unionSet[i]);
+    printf("\nIntersection (A ∩ B) Bit String: ");
+    for(i = 0; i < nU; i++) printf("%d", intersectionSet[i]);
+    printf("\nDifference (A - B) Bit String: ");
+    for(i = 0; i < nU; i++) printf("%d", differenceSet[i]);
+    printf("\n\nUnion (A ∪ B) = { ");
+    for(i = 0; i < nU; i++) if(unionSet[i]) printf("%d ", U[i]);
+    printf("}");
+    printf("\nIntersection (A ∩ B) = { ");
+    for(i = 0; i < nU; i++) if(intersectionSet[i]) printf("%d ", U[i]);
+    printf("}");
+    printf("\nDifference (A - B) = { ");
+    for(i = 0; i < nU; i++) if(differenceSet[i]) printf("%d ", U[i]);
+    printf("}\n");
+    return 0;
+}
