1821559fb771b3b305f8694f4e0acdf47813b96c
mithun-mp <mithun.m.p.mail@gmail.com>
2025-10-21 14:17:37 +0530

    Update 8-disjoint-set.c

--- Semester1/DataStructure/8-disjoint-set.c
+++ Semester1/DataStructure/8-disjoint-set.c
@@ -3,10 +3,11 @@
 
 int parent[MAX];
 
-void create(int n) {
+void makeSet(int n) {
     for(int i = 1; i <= n; i++) {
         parent[i] = i;
     }
+    printf("Created %d disjoint sets.\n", n);
 }
 
 int find(int x) {
@@ -19,33 +20,39 @@ int find(int x) {
 void unionSets(int a, int b) {
     int rootA = find(a);
     int rootB = find(b);
-    if(rootA != rootB)
+    if(rootA != rootB) {
         parent[rootB] = rootA;
+        printf("Union of %d and %d done.\n", a, b);
+    } else {
+        printf("%d and %d are already in the same set.\n", a, b);
+    }
 }
 
 int main() {
     int n, u, v, choice;
 
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
-    create(n);
-
     while(1) {
-        printf("\n1. Union\n2. Find\n3. Exit\nChoose operation: ");
+        printf("\n--- Disjoint Set Operations ---\n");
+        printf("1. Make Set\n2. Union\n3. Find\n4. Exit\nChoose operation: ");
         scanf("%d", &choice);
 
         if(choice == 1) {
+            printf("Enter number of elements: ");
+            scanf("%d", &n);
+            makeSet(n);
+        }
+        else if(choice == 2) {
             printf("Enter two elements to union: ");
             scanf("%d %d", &u, &v);
             unionSets(u, v);
-            printf("Union done.\n");
         }
-        else if(choice == 2) {
+        else if(choice == 3) {
             printf("Enter element to find its set: ");
             scanf("%d", &u);
             printf("Representative of %d is %d\n", u, find(u));
         }
-        else if(choice == 3) {
+        else if(choice == 4) {
+            printf("Exiting program.\n");
             break;
         }
         else {
